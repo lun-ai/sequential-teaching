43a2f07d9c29f2bbb2af1ecce1765aa86f1616e4
Lun Ai <allenai1997@vip.sina.com>
2022-03-01 15:58:18 +0000

    [LA] added records for group 2 and 4

--- data/analysis/analysis.py
+++ data/analysis/analysis.py
@@ -26,8 +26,10 @@ DATA_DIR_TEST1_GROUP3 = "../test/test_1/Group3/"
 DATA_DIR_TEST1_GROUP4 = "../test/test_1/Group4/"
 DATA_DIR_TEST1_ANOMALY = "../test/test_1/anomaly/"
 DATA_DIR_AMT_GROUP1 = "../test/amt/Group1/"
+DATA_DIR_AMT_GROUP2 = "../test/amt/Group2/"
 DATA_DIR_AMT_GROUP3 = "../test/amt/Group3/"
-DATA_DIR_TEST1_ANOMALY = "../test/amt/anomaly/"
+DATA_DIR_AMT_GROUP4 = "../test/amt/Group4/"
+DATA_DIR_AMT_ANOMALY = "../test/amt/anomaly/"
 
 # extract_from_CSV([DATA_DIR_TEST1_GROUP1], sim='chi_sq', show_sim=True, sim_graphs=True, save_graph=True)
 # extract_from_CSV([DATA_DIR_TEST1_GROUP2], sim='chi_sq', show_sim=True, sim_graphs=True, save_graph=True)
@@ -39,7 +41,10 @@ DATA_DIR_TEST1_ANOMALY = "../test/amt/anomaly/"
 # g3 = extract_from_CSV([DATA_DIR_TEST1_GROUP3], sim='chi_sq_2x2', show_sim=True, sim_graphs=True, save_graph=True, significance=0.01)
 # g4 = extract_from_CSV([DATA_DIR_TEST1_GROUP4], sim='chi_sq_2x2', show_sim=True, sim_graphs=True, save_graph=True)
 
-extract_from_CSV([DATA_DIR_AMT_GROUP3], sim='chi_sq_2x2', show_sim=True, sim_graphs=False, verbose=False)
+# extract_from_CSV([DATA_DIR_AMT_GROUP1], sim='chi_sq_2x2', show_sim=True, sim_graphs=False, verbose=False)
+# extract_from_CSV([DATA_DIR_AMT_GROUP2], sim='chi_sq_2x2', show_sim=True, sim_graphs=False, verbose=False)
+# extract_from_CSV([DATA_DIR_AMT_GROUP3], sim='chi_sq_2x2', show_sim=True, sim_graphs=False, verbose=False)
+extract_from_CSV([DATA_DIR_AMT_GROUP4], sim='chi_sq_2x2', show_sim=True, sim_graphs=False, verbose=False)
 # extract_from_CSV([DATA_DIR_AMT_GROUP1], sim='chi_sq_2x2', show_sim=True, sim_graphs=True, save_graph=True, save_path="../results/amt/", significance=0.01)
 
 # t_test_with_graph([[np.array(g1["sort_test_comp"])[:, :5].flatten(),
